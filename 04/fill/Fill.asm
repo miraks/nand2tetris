82625d06260b5862e60cfffa3e730b9028eef71e
miraks <a@vldkn.net>
2018-04-23 00:29:54 +0300

    Week 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,48 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(READ)
+  @color
+  M=0
+  @KBD
+  D=M
+  @PAINT
+  D;JEQ
+
+  @color
+  M=-1
+
+(PAINT)
+  @8191
+  D=A
+  @i
+  M=D
+  @LOOP
+  0;JMP
+
+(LOOP)
+  @SCREEN
+  D=A
+  @i
+  D=D+M
+  @pos
+  M=D
+  @color
+  D=M
+  @pos
+  A=M
+  M=D
+
+  @i
+  D=M
+  @READ
+  D;JEQ
+  @i
+  M=M-1
+  @LOOP
+  0;JMP
